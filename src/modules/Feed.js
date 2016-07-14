f01a2d00ff0622ef9493fe570c6f343376fe61b7
dassiShenkar <dassi.shenkar@gmail.com>
2016-07-14 15:20:21 +0300

    feed logic is working

--- src/modules/Feed.js
+++ src/modules/Feed.js
@@ -6,13 +6,17 @@ import '../styles/components/feed.scss';
 export default class Feed extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {parentData: [], sitterData: []};
+        this.state = {parentData: [], availableSitters: [], favoriteSitters: [], topSitters: []};
     }
 
     componentDidMount() {
         this.loadParentFromServer();
-        this.loadSittersFromServer();
-        setInterval(this.loadParentFromServer.bind(this), 2000);
+        this.loadAvailableSittersFromServer();
+        this.loadParentFavoriteSittersFromServer();
+        this.loadTopSittersFromServer();
+        setInterval(this.loadParentFavoriteSittersFromServer.bind(this), 3000);
+        setInterval(this.loadAvailableSittersFromServer.bind(this), 3000);
+        setInterval(this.loadTopSittersFromServer.bind(this), 3000);
     }
 
     loadParentFromServer() {
@@ -31,12 +35,41 @@ export default class Feed extends React.Component {
         });
     }
 
-    loadSittersFromServer() {
+    loadAvailableSittersFromServer() {
         $.ajax({
             url: 'https://sitters-ws.herokuapp.com/getAvailableNowSitters',
             dataType: 'json',
             success: function (data) {
-                this.setState({sitterData: data});
+                this.setState({availableSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    loadParentFavoriteSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getParentFavoriteSitters',
+            dataType: 'json',
+            type : 'post',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'email': 'parent1@gmail.com'}),
+            success: function (data) {
+                this.setState({favoriteSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    loadTopSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getTopRatedSitters',
+            dataType: 'json',
+            success: function (data) {
+                this.setState({topSitters: data});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -54,7 +87,9 @@ export default class Feed extends React.Component {
                         <h3 className="name">{this.state.parentData.name}</h3>
                     </section>
                 </header>
-                <SitterList id="sitters-available-now" title="available-now" sittersData={this.state.sitterData}/>
+                <SitterList id="sitters-available-now" title="Available Now" sittersData={this.state.availableSitters}/>
+                <SitterList id="sitters-favorites" title="My Favorites" sittersData={this.state.favoriteSitters}/>
+                <SitterList id="sitters-top-rated" title="Top Rated" sittersData={this.state.topSitters}/>
             </div>
         );
     }
