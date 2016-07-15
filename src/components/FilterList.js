13fff914609b72dcaf8b96b22131443cd0ec5e22
dassiShenkar <dassi.shenkar@gmail.com>
2016-07-15 18:23:34 +0300

    filter is now getting filtered data from the server

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StarRatingComponent from 'react-star-rating-component';
+import SitterList from '../components/SitterList';
 import '../styles/components/filters.scss';
 
 export default class FilterList extends React.Component {
@@ -9,32 +10,117 @@ export default class FilterList extends React.Component {
             selectedFilter: "availableNow",
             rating: 5,
             workingHours: "mornings",
-            gender: "male"
-
+            gender: "male",
+            availableSitters: null,
+            favoriteSitters: null,
+            topSitters: null,
+            sittersByGender: null,
+            sittersByHours: null,
+            allSitters: null
         }
+    }
+
+    componentDidMount() {
+        this.loadAvailableSittersFromServer();
+    }
+
+    loadAllSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getAllSitters',
+            dataType: 'json',
+            success: function (data) {
+                this.setState({allSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    loadAvailableSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getAvailableNowSitters',
+            dataType: 'json',
+            success: function (data) {
+                this.setState({availableSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    loadTopSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getTopRatedSitters',
+            dataType: 'json',
+            success: function (data) {
+                this.setState({topSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    loadParentFavoriteSittersFromServer() {
+        $.ajax({
+            url: 'https://sitters-ws.herokuapp.com/getParentFavoriteSitters',
+            dataType: 'json',
+            type : 'post',
+            contentType: 'application/json',
+            data: JSON.stringify({ 'email': 'parent1@gmail.com'}),
+            success: function (data) {
+                this.setState({favoriteSitters: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString()); //TODO: remove console.log
+            }.bind(this)
+        });
     }
 
     onChange(filterName) {
         switch (filterName) {
             case "availableNow":
-                this.setState({selectedFilter : "availableNow"});
+                this.setState({selectedFilter: "availableNow"});
+                if(!this.state.availableSitters) {
+                    this.loadAvailableSittersFromServer();
+                }
                 break;
             case "rating":
-                this.setState({selectedFilter : "rating"});
+                this.setState({selectedFilter: "rating"});
+                this.state.availableSitters = this.loadAvailableSittersFromServer();
                 break;
             case "workingHours":
-                this.setState({selectedFilter : "workingHours"});
+                this.setState({selectedFilter: "workingHours"});
                 break;
             case "favorites":
-                this.setState({selectedFilter : "favorites"});
+                this.setState({selectedFilter: "favorites"});
                 break;
             case "gender":
-                this.setState({selectedFilter : "gender"});
+                this.setState({selectedFilter: "gender"});
                 break;
             case "seeAll":
-                this.setState({selectedFilter : "seeAll"});
+                this.setState({selectedFilter: "seeAll"});
+                this.state.allSitters = this.loadAllSittersFromServer();
+                break;
+            case "mornings":
+                this.setState({selectedFilter: "mornings"});
+                break;
+            case "evenings":
+                this.setState({selectedFilter: "evenings"});
+                break;
+            case "allDay":
+                this.setState({selectedFilter: "allDay"});
+                break;
+            case "male":
+                this.setState({selectedFilter: "male"});
+                break;
+            case "female":
+                this.setState({selectedFilter: "female"});
                 break;
         }
+
     }
 
     onHoursChange(timeOfDay) {
@@ -66,7 +152,30 @@ export default class FilterList extends React.Component {
         this.setState({rating: value});
     }
 
+    onFilterChange() {
+        console.log("boom!");
+    }
+
     render() {
+        let available, favorites, topRated, workingHours, gender, seeAll;
+        if(this.state.availableSitters && this.state.selectedFilter === "availableNow") {
+            available = <SitterList className="sitters-available-now" title="AVAILABLE NOW" sittersData={this.state.availableSitters}/>;
+        }
+        if(this.state.topSitters && this.state.selectedFilter === "availableNow") {
+            topRated = <SitterList className="sitters-top-rated" title="TOP RATED" sittersData={this.state.topSitters}/>;
+        }
+        if(this.state.sittersByHours && this.state.selectedFilter === "availableNow") {
+            workingHours = <SitterList className="sitters-working-hours" title="WORKING HOURS" sittersData={this.state.sittersByHours}/>;
+        }
+        if(this.state.favoriteSitters && this.state.selectedFilter === "availableNow") {
+            favorites = <SitterList className="sitters-favorites" title="MY FAVORITES" sittersData={this.state.favoriteSitters}/>;
+        }
+        if(this.state.sittersByGender && this.state.selectedFilter === "availableNow") {
+            gender = <SitterList className="sitters-gender" title="GENDER" sittersData={this.state.sittersByGender}/>;
+        }
+        if(this.state.allSitters && this.state.selectedFilter === "seeAll") {
+            seeAll = <SitterList className="sitters-see-all" title="ALL" sittersData={this.state.allSitters}/>;
+        }
         return (
             <div className="filter-list" onChange={this.onChange.bind(this)}>
                 <h1>Sort by</h1>
@@ -109,33 +218,37 @@ export default class FilterList extends React.Component {
                 {this.state.selectedFilter === "workingHours"? <section className="working-hours-box"><h3>Working Hours</h3>
                     <div>
                         <label htmlFor="morning">Mornings
-                            <input id="morning" value="morning" type="radio" name="working-hours-radio" onChange={this.onHoursChange.bind(this, "mornings")}/>
+                            <input id="morning" value="morning" type="radio" name="working-hours-radio" onChange={this.onChange.bind(this, "mornings")}/>
                         </label>
                     </div>
                     <div>
                         <label htmlFor="evening">Evenings
-                            <input id="evening" value="evening" type="radio" name="working-hours-radio" onChange={this.onHoursChange.bind(this, "evenings")}/>
+                            <input id="evening" value="evening" type="radio" name="working-hours-radio" onChange={this.onChange.bind(this, "evenings")}/>
                         </label>
                     </div>
                     <div>
                         <label htmlFor="all-day">All day
-                            <input id="all-day" value="all-day" type="radio" name="working-hours-radio" onChange={this.onHoursChange.bind(this, "allDay")}/>
+                            <input id="all-day" value="all-day" type="radio" name="working-hours-radio" onChange={this.onChange.bind(this, "allDay")}/>
                         </label>
                     </div>
                 </section> : null}
                 {this.state.selectedFilter === "gender"? <section className="gender-box"><h3>Gender</h3>
                     <div>
                         <label htmlFor="male">Male
-                            <input id="male" value="male" type="radio" name="gender-radio" onChange={this.onGenderChange.bind(this, "male")}/>
+                            <input id="male" value="male" type="radio" name="gender-radio" onChange={this.onChange.bind(this, "male")}/>
                         </label>
                     </div>
                     <div>
                         <label htmlFor="female">Female
-                            <input id="female" value="female" type="radio" name="gender-radio" onChange={this.onGenderChange.bind(this, "female")}/>
+                            <input id="female" value="female" type="radio" name="gender-radio" onChange={this.onChange.bind(this, "female")}/>
                         </label>
                     </div>
                 </section> : null}
-                <div class="btn btn-primary" onClick={this.props.onFilterChange}>Save filters</div>
+                <div class="btn btn-primary" onClick={this.onFilterChange}>Save filters</div>
+                {available}
+                {favorites}
+                {topRated}
+                {seeAll}
             </div>
         );
     }
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ProfileImg from '../components/ProfileImg';
-import SitterList from '../components/SitterList';
 import FilterList from '../components/FilterList';
 import Filter from '../styles/icons/Filter';
 import Calendar from '../styles/icons/Calendar';
@@ -10,30 +9,7 @@ export default class Feed extends React.Component {
     constructor(props) {
         super(props);
         this.state = {parentData: {name: localStorage.name, email: localStorage.email,
-            profilePictureURL: localStorage.profilePicture}, availableSitters: null, favoriteSitters: null,
-            topSitters: null, showFilter: false};
-    }
-
-    componentDidMount() {
-        this.loadAvailableSittersFromServer();
-        // this.loadParentFavoriteSittersFromServer();
-        // this.loadTopSittersFromServer();
-        // setInterval(this.loadParentFavoriteSittersFromServer.bind(this), 3000);
-        setInterval(this.loadAvailableSittersFromServer.bind(this), 3000);
-        // setInterval(this.loadTopSittersFromServer.bind(this), 3000);
-    }
-
-    loadAvailableSittersFromServer() {
-        $.ajax({
-            url: 'https://sitters-ws.herokuapp.com/getAvailableNowSitters',
-            dataType: 'json',
-            success: function (data) {
-                this.setState({availableSitters: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
+            profilePictureURL: localStorage.profilePicture}, showFilter: false};
     }
 
     loadParentFavoriteSittersFromServer() {
@@ -52,40 +28,13 @@ export default class Feed extends React.Component {
         });
     }
 
-    loadTopSittersFromServer() {
-        $.ajax({
-            url: 'https://sitters-ws.herokuapp.com/getTopRatedSitters',
-            dataType: 'json',
-            success: function (data) {
-                this.setState({topSitters: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    }
-
     onFilterClick(e) {
         e.preventDefault();
         this.setState({showFilter : !this.state.showFilter});
     }
-
-    onFilterChange() {
-        console.log("meu");
-    }
     
-
+    
     render() {
-        let available, favorites, topRated;
-        if(this.state.availableSitters) {
-            available = <SitterList className="sitters-available-now" title="AVAILABLE NOW" sittersData={this.state.availableSitters}/>;
-        }
-        if(this.state.favoriteSitters) {
-            favorites = <SitterList className="sitters-favorites" title="MY FAVORITES" sittersData={this.state.favoriteSitters}/>;
-        }
-        if(this.state.topSitters) {
-            topRated = <SitterList className="sitters-top-rated" title="TOP RATED" sittersData={this.state.topSitters}/>;
-        }
         return (
             <div id="feed">
                 <header>
@@ -102,9 +51,6 @@ export default class Feed extends React.Component {
                     </section>
                 </header>
                 {this.state.showFilter ? <FilterList onFilterChange={this.onFilterChange} className="filter"/> : null}
-                {available}
-                {favorites}
-                {topRated}
             </div>
         );
     }
